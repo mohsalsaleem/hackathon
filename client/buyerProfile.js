7da2b05fd6dcad3763805f7611a421985df72792
Mohamed Saleem <mohsal.saleem@gmail.com>
2015-02-22 04:47:48 +0530

    Added Geo

--- client/buyerProfile.js
+++ client/buyerProfile.js
@@ -2,7 +2,18 @@
 
 Template.buyerProfile.helpers({
     'feed':function(){
-        return AdDetails.find({});
+        return AdDetails.find({},{$selector:{"Location":"adayar"}});
+    },
+    'loc':function(){
+        var location =  Geolocation.latLng()
+        var lati = location.lat;
+        var long = location.lng;
+        //console.log(lati);
+      
+        return location;
+        
+        
+        
     }
 })
 
